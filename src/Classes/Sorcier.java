fbcc64b2cef71a61056d6a490fa1e6f31119d0a4
TO Jeremy <jeremy.to@etu.sorbonne-universite.fr>
2024-02-11 14:14:57 +0100

    Ajout de classes personnages

---
+++ src/Classes/Sorcier.java
@@ -0,0 +1,9 @@
+package Classes;
+
+import Personnages.Personnage;
+
+public class Sorcier extends Personnage {
+    public Sorcier(String nomPerso, int maxHp, int maxMana, int attaque, int defense) {
+        super(nomPerso, maxHp, maxMana, attaque, defense);
+    }
+}
